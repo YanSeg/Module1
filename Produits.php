ae351878fa9dde0481e3d9f86633fe2d3f7b1e50
YanSeg <yan.seguin@le-campus-numerique.fr>
2023-05-26 14:34:48 +0200

    premier commit

---
+++ Produits.php
@@ -0,0 +1,33 @@
+<?php 
+
+$produitDog = [
+    'nom' => Dog,
+    'prix' => 300,
+    'image' => "https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg";
+]
+
+
+
+
+$name = "dog";
+$prix = 300;
+$image = "https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg";
+
+
+$name = "chat";
+$prix = 300;
+$image = "https://cdn.pixabay.com/photo/2023/05/23/15/26/bengal-cat-8012976_1280.jpg";
+
+
+
+
+$name = "cocinelle";
+$prix = 10;
+$image = "https://cdn.pixabay.com/photo/2023/05/24/18/32/ladybug-8015502_1280.jpg";
+
+
+
+
+
+
+?>
