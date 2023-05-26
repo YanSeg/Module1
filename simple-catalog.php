31931671c95e600487391438a23ce2195ac4fdf2
YanSeg <yan.seguin@le-campus-numerique.fr>
2023-05-26 15:29:02 +0200

    ajout du fichier simple-catalogue

--- simple-catalog.php
+++ simple-catalog.php
@@ -1,11 +1,43 @@
+<?php 
+$lesproduits = [
+
+[
+    'nom'=> "Dog",
+    'prix'=> 300,
+    'image' => "https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg"
+],
+
+[
+
+
+'nom' => "dog",
+'prix' => 300,
+'image' =>"https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg"
+
+],
+
+[
+'nom' => "chat",
+'prix' => 300,
+'image' => "https://cdn.pixabay.com/photo/2023/05/23/15/26/bengal-cat-8012976_1280.jpg"
+
+]
+];
+
+print_r ($lesproduits);
+?>
+
+</br>
+
 <?php
 
-include 'Produits';
-
-$products = [chien, dog, cocinelle]
-
-
+$Try = [ "l", "p" , "m", "k", "z"];
+sort($Try);
+print_r ($Try);
 
 
+print_r ($Try[0]);
 
+
+print_r ($Try[4]);
 ?>
