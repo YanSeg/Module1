4894f40db2e12593d26fe514112930aa46701a6c
YanSeg <yan.seguin@le-campus-numerique.fr>
2023-05-26 17:12:48 +0200

    debut des fonctions

--- Produits.php
+++ Produits.php
@@ -4,7 +4,8 @@ $lesproduits = [
 [
     'nom'=> "Dog",
     'prix'=> 300,
-    'image' => "https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg"
+    'image' => "https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg",
+    'stock' =>0
 ],
 
 [
@@ -12,14 +13,16 @@ $lesproduits = [
 
 'nom' => "dog",
 'prix' => 300,
-'image' =>"https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg"
+'image' =>"https://cdn.pixabay.com/photo/2023/05/21/12/40/dog-8008483_1280.jpg",
+'stock' =>0
 
 ],
 
 [
 'nom' => "chat",
 'prix' => 300,
-'image' => "https://cdn.pixabay.com/photo/2023/05/23/15/26/bengal-cat-8012976_1280.jpg"
+'image' => "https://cdn.pixabay.com/photo/2023/05/23/15/26/bengal-cat-8012976_1280.jpg",
+'stock' =>0
 
 ]
 ];
@@ -30,6 +33,8 @@ echo '</pre>';
 ?>
 
 </br>
+
+
 
 <?php
 
@@ -42,4 +47,26 @@ print_r ($Try[0]);
 
 
 print_r ($Try[4]);
+
+while ($lesproduits[0]['stock']<10)
+{ echo 'PLUS EN STOCK <br/>';
+    $lesproduits[0]['stock']=$lesproduits[0]['stock']+1 ; 
+;}
+
+//break; pour sortir de la boucle même si la condition n'est pas remplie 
+
+for ($nbligne=0; $nbligne<10; $nbligne++ ) {
+    echo 'encore une ! <br/>';
+}
+
+
+$lettreAlphabet = ["a","b","c","d","e","f","g","h","i","j"];
+
+for ($i =0;$i<count($lettreAlphabet); $i=$i+1)
+{
+    echo '-' .  $lettreAlphabet[$i] . "\n";
+   
+}
+
+
 ?>
